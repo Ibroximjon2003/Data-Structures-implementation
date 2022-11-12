84993a2cbfbb387b72b4384e747930dff5310ebd
unknown <maxammadjonovibroximjon24@gmail.com>
2022-11-12 17:11:11 +0500

    array

---
+++ src/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+
+
+/**
+ * 
+ */
+public static void array(){
+       //implementation of one dimensional arrays
+   int[] integerNumbers = {1,2,3,4}; // first way to declare array, mostly used
+   //integerNumbers = new int[4]; // initialization of array
+   integerNumbers[0] = 5;
+
+  }
+
+}
