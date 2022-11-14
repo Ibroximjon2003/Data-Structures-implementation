715713a04c2e561a622bff492ae13940ac8b9b34
unknown <maxammadjonovibroximjon24@gmail.com>
2022-11-14 10:22:53 +0500

    linked list

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-       new Arrays();
-       int[] arr1 = {1,2,3,4,5,6,7,7,8};
-       Arrays.oddArray(arr1);
-    }
-}
--- src/arrays.java
+++ src/arrays.java
@@ -1,6 +1,7 @@
 public class Arrays {
 
-public static void oddArray(int[] arr){ // this is the function which divides odd numbers of array
+  // this is the function which divides odd numbers of array
+public static void oddArray(int[] arr){ 
   int oddCount = 0;
   for (int i = 0; i < arr.length; i++) {
     if (arr[i]%2==1) {
@@ -15,7 +16,32 @@ public static void oddArray(int[] arr){ // this is the function which divides od
     idx++;
   }
  }
-
+System.out.println(result);
 }
+// Reverse an array
+public void reverseArray(int[] numbers, int start, int end) {
+  while(start<end){
+    int temp = numbers[start];
+    numbers[start] = numbers[end];
+    numbers[end] = temp;
+    start++;
+    end--;
+    
+  }
+  System.out.println(numbers);
+}
+// finding minimum value in array
+public int  findingMinimumArray(int arr[]) {
+  int min = arr[0];
+  for(int i = 1; i<arr.length; i++){
+    if(arr[i]<min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+
+
 
 }
